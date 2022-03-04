9e1b62c0971b50bdc361c00071b6e2bcab876be4
Fabio Speroni <fabio.speroni@gmail.com>
2022-03-03 22:57:12 -0700

    worked on schema

---
+++ backend/host_a_pet_schema.sql
@@ -0,0 +1,56 @@
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL
+    CHECK (position('@' IN email) > 1),
+  phone TEXT NOT NULL,
+  postal_code TEXT NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE pets (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  type TEXT NOT NULL,
+  photo TEXT NOT NULL,
+  owner_id INTEGER NOT NULL
+    REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE hostings (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  host_id INTEGER NOT NULL
+    REFERENCES users ON DELETE CASCADE,
+  pet_id INTEGER NOT NULL
+    REFERENCES pets ON DELETE CASCADE
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  rating INTEGER NOT NULL,
+  comments TEXT,
+  reviewer_id INTEGER NOT NULL
+    REFERENCES users ON DELETE CASCADE,
+  hosting_id INTEGER NOT NULL
+    REFERENCES hostings ON DELETE CASCADE
+);
+
+CREATE TABLE availabilities (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  user_id INTEGER NOT NULL
+    REFERENCES users ON DELETE CASCADE,
+  role_id INTEGER NOT NULL
+    REFERENCES roles ON DELETE CASCADE
+)
