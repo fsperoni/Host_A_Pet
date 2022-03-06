234d63757d915f298c18658952d91066bd15bc7e
Fabio Speroni <fabio.speroni@gmail.com>
2022-03-05 18:01:54 -0700

    worked on variable login info

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,29 +7,30 @@ import '../styles/Header.scss';
 
 const Header = () => {
   // const { currentUser } = useContext(UserContext);
+  const logged = false;
   return (
-    <>
-    {/* <header className="headerContainer"> */}
-      <Navbar expand="md" direction="horizontal">
-        <NavLink end to="/" className="navbar-brand">
-          <div className="d-flex">
-            <img src={logoImg} alt="Host a Pet"/>
-            <h1 className="my-auto">Host a Pet</h1>
-          </div>
-        </NavLink>
-        <Nav navbar className="ms-5">
-          <NavItem>
-            <NavLink className="mr-2" end to="/">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="mr-2" end to="/loxa">Home</NavLink>
-          </NavItem>
-        </Nav>
-        <div className="login">
+    <Navbar expand="md" direction="horizontal">
+      <NavLink end to="/" className="navbar-brand">
+        <div className="d-flex">
+          <img src={logoImg} alt="Host a Pet"/>
+          <h1 className="my-auto">Host a Pet</h1>
+        </div>
+      </NavLink>
+      <Nav navbar className="ms-5">
+        <NavItem>
+          <NavLink className="mr-2" end to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className="mr-2" end to="/loxa">Home</NavLink>
+        </NavItem>
+      </Nav>
+      <div className="login">
+        {logged ? 
+          <a href="#">Logged</a> :
           <a href="#">Login stuff</a>
-        </div> 
-      </Navbar>
-  </>
+        }
+      </div> 
+    </Navbar>
   );
 }
 
